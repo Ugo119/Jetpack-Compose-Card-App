d2a6575af84b6902e9bb6d05d74b83ac5aad004a
Ugo119 <onuoha.ugochukwu@yahoo.com>
2021-09-16 13:21:17 +0100

    added logic to show snackbar with text from TextField

--- app/src/main/java/com/ugo/android/firstjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/ugo/android/firstjetpackcompose/MainActivity.kt
@@ -9,11 +9,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -26,68 +25,54 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ugo.android.firstjetpackcompose.ui.theme.FirstJetpackComposeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val painter = painterResource(id = R.drawable.cute_boy_two)
-            val description = "Image of a cute little boy"
-            val title = "Jayden is looking fly!"
-            Box(modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .padding(16.dp)) {
-                ImageCard(painter = painter,
-                    contentDescription = description,
-                    title = title)
-            }
+           val scaffoldState = rememberScaffoldState()
+           var textFieldState by remember {
+               mutableStateOf("")
+           }
+           val scope = rememberCoroutineScope()
+           Scaffold(
+               modifier = Modifier.fillMaxSize(),
+               scaffoldState = scaffoldState
+           ) {
+               Column(
+                   horizontalAlignment = Alignment.CenterHorizontally,
+                   verticalArrangement = Arrangement.Center,
+                   modifier = Modifier.fillMaxSize()
+               ) {
+                   TextField(
+                       value = textFieldState ,
+                       label = {
+                           Text("Enter your name")
+                       },
+                       onValueChange = {
+                           textFieldState = it
+                       },
+                       singleLine = true,
+                       //modifier = Modifier.fillMaxWidth()
+                   )
+                   Spacer(modifier = Modifier.height(20.dp))
+                   Button(
+                       onClick = {
+                           scope.launch {
+                               scaffoldState.snackbarHostState.showSnackbar("Hello again, $textFieldState")
+                           }
+                       }) {
+                       Text(text = "Click to greet")
+                   }
+               }
+
+           }
         }
     }
 }
 
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(modifier = Modifier
-        .fillMaxWidth(),
-         shape = RoundedCornerShape(15.dp),
-         elevation = 15.dp) {
-        Box(modifier = Modifier
-            .height(200.dp)) {
-            Image(painter = painter, 
-                contentDescription = contentDescription, 
-                contentScale = ContentScale.Crop
-            )
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    Brush.verticalGradient(
-                        colors = listOf(
-                            Color.Transparent,
-                            Color.Black
-                        ),
-                        startY = 200f
-                    )
-                )) {
-                
-            }
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-                contentAlignment = Alignment.BottomStart
-            ) {
-                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
-            }
-            
-        }
 
-    }
-
-}
 
 
 
