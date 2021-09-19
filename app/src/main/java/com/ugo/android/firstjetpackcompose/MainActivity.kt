b60ec3271e4ff5a2dd8cd66dd9f40ad70e6fd511
Ugo119 <onuoha.ugochukwu@yahoo.com>
2021-09-19 14:04:41 +0100

    Trying to learn more about side effect

--- app/src/main/java/com/ugo/android/firstjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/ugo/android/firstjetpackcompose/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.ugo.android.firstjetpackcompose
 
+import android.content.ContentValues.TAG
 import android.inputmethodservice.Keyboard
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -9,11 +11,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -26,68 +28,49 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ugo.android.firstjetpackcompose.ui.theme.FirstJetpackComposeTheme
+import kotlinx.coroutines.launch
 
+//Side effects are those things, though within a compose function but not a composable and have nothing
+// to do with compose eg incrementing a counter or making network calls. They are those things that
+// are not within scope of a function.
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val painter = painterResource(id = R.drawable.cute_boy_two)
-            val description = "Image of a cute little boy"
-            val title = "Jayden is looking fly!"
-            Box(modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .padding(16.dp)) {
-                ImageCard(painter = painter,
-                    contentDescription = description,
-                    title = title)
-            }
+            CounterComposable()
         }
     }
 }
 
 @Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(modifier = Modifier
-        .fillMaxWidth(),
-         shape = RoundedCornerShape(15.dp),
-         elevation = 15.dp) {
-        Box(modifier = Modifier
-            .height(200.dp)) {
-            Image(painter = painter, 
-                contentDescription = contentDescription, 
-                contentScale = ContentScale.Crop
-            )
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    Brush.verticalGradient(
-                        colors = listOf(
-                            Color.Transparent,
-                            Color.Black
-                        ),
-                        startY = 200f
-                    )
-                )) {
-                
+fun CounterComposable() {
+    val scaffoldState = rememberScaffoldState()
+    var counter = remember {
+        0
+    }
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        modifier = Modifier
+        .fillMaxSize(),
+        scaffoldState = scaffoldState) {
+
+        Button(onClick = {
+            counter++
+            Log.e(TAG, "CounterComposable: $counter")
+        }) {
+            Text(text = "Click me! $counter")
+        }
+        if (counter > 0 && counter % 5 == 0) {
+            LaunchedEffect(scaffoldState.snackbarHostState){
+                scaffoldState.snackbarHostState.showSnackbar("Item $counter")
             }
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-                contentAlignment = Alignment.BottomStart
-            ) {
-                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
-            }
-            
         }
 
     }
 
 }
+
 
 
 
